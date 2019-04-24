2caa56f70489366c161ca3a1d3a15fff3ef43be6
Biao Huang <hwangbiao@gmail.com>
2019-04-24 22:12:01 +0800

    run python codes within Atom using 'script'; check the version & command

---
+++ python_runSetup.py
@@ -0,0 +1,17 @@
+
+import sys
+# to check the python version
+# and the command location
+
+print(sys.version)
+print(sys.executable)
+
+
+# run the code under conda virtual environment py3.6
+# the shortcut on Mac is command + shift + K
+#
+
+# command + shift + P --> script --> run option
+# --> command: /anaconda3/envs/py3.6/bin/python
+# --> save as profile --> profile name: py3.6
+# in a similar way, another virtual environment can be set.
